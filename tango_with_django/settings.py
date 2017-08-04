9a9c947922efd7bec2b6cbe64660307be930cc3d
jxltom <jxltom@gmail.com>
2017-08-04 10:59:10 +0800

    Add serving media files

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -122,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -1,6 +1,6 @@
-"""tango_with_django URL Configuration."""
-
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from rango import views
@@ -10,4 +10,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
