79b393f00b5b25ade220b20c0728210b35fa4a46
jxltom <jxltom@gmail.com>
2017-08-04 19:14:31 +0800

    Add top 5 categories list in index page

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from rango.models import Category
+
 
 def index(request):
-    ctx_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    ctx_dict = {'categories': Category.objects.order_by('-likes')[:5]}
     return render(request, 'rango/index.html', context=ctx_dict)
 
 
