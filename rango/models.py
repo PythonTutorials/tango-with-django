d58ee451629d42cdd30ad65d8ec797e6186cc5ec
jxltom <jxltom@gmail.com>
2017-08-05 21:30:19 +0800

    Avoid repetition of max_length in forms

--- rango/models.py
+++ rango/models.py
@@ -4,7 +4,8 @@ from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     """Category model."""
-    name = models.CharField(max_length=128, unique=True)
+    name_max_length = 128
+    name = models.CharField(max_length=name_max_length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -24,7 +25,8 @@ class Page(models.Model):
     """Page model."""
     category = models.ForeignKey(Category)
 
-    title = models.CharField(max_length=128)
+    title_max_length = 128
+    title = models.CharField(max_length=title_max_length)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
